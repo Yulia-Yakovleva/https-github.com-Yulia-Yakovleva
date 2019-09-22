c8a94480fd6bb765f0ebe255873f58ed7a3f953c
Yulia-Yakovleva <st041958@student.spbu.ru>
2019-09-22 13:19:35 +0300

    Добавлен скрип-калькулятор

---
+++ calculator.py
@@ -0,0 +1,26 @@
+a = float(input())
+c = input()
+b = float(input())
+if c == '+':
+    print(a + b)
+elif c == '-':
+    print(a - b)
+elif c == '*':
+    print(a * b)
+elif c == '/':
+    if b == 0.0:
+        print('Деление на 0!')
+    if b != 0.0:
+        print(a / b)
+elif c == 'mod':
+    if b == 0.0:
+        print('Деление на 0!')
+    if b != 0.0:
+        print(a % b)
+elif c == 'pow':
+    print(a ** b)
+elif c == 'div':
+    if b == 0.0:
+        print('Деление на 0!')
+    if b != 0.0:
+        print(a // b)
