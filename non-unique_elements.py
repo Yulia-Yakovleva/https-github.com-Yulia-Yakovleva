c29946efc053eb19590406078ecb319b5535818b
Yulia-Yakovleva <st041958@student.spbu.ru>
2019-10-01 20:32:57 +0300

    Финальная версия!!1одынодын

--- non-unique_elements.py
+++ non-unique_elements.py
@@ -1,16 +1,18 @@
 massive = [0]
-repetitive = []
+
+
+def checkio(request):
+    repetitive = []
+    for element in request:
+        if request.count(element) > 1:
+            repetitive.append(element)
+    return repetitive
+
+
 while massive[0] != 'stop':
-    print('Программа запущена! Для выхода из программы введите stop, а пока...\nВедите непустой массив целых чисел:')
+    print('Программа запущена! Для выхода из программы введите stop, а пока...'
+          '\nВедите непустой массив целых чисел:')
     massive = [(i) for i in input().split()]
-
-    def checkio(s):
-        for element in massive:
-            if massive.count(element) > 1:
-                repetitive.append(element)
-                return repetitive
-
-    checkio(massive)
-    print('Неуникальные элементы данного массива: ' + str(repetitive))
+    print('Неуникальные элементы данного массива: ' + str(checkio(massive)))
 else:
     print('Программа завершила работу.')
