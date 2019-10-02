e24d19405bc3c2edbe426bb9b501e1c8f25a220a
Yulia-Yakovleva <st041958@student.spbu.ru>
2019-10-02 15:22:59 +0300

    Убрала лишние строки в коде

--- flatten.py
+++ flatten.py
@@ -1,7 +1,5 @@
 def flat_list(request):
     result = []
-    if not isinstance(request, (list, tuple)):  # если неправда, что запрос - это список или кортеж, то
-        return request                          # верни запрос (ведь все нормуль)
     for element in request:                     # для каждого элемента в запросе
         if isinstance(element, (list, tuple)):  # если элемент - это список или кортеж,то
             # расширяет результат, добавляя в конец результат применения функции c элементом в качестве аргумента
