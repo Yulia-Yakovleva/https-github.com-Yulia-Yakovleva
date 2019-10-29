75dde1a52ffa1a121291fbfd863b55d75367232d
Yulia-Yakovleva <49680968+Yulia-Yakovleva@users.noreply.github.com>
2019-10-29 23:42:22 +0300

    Update blast_proteins.py

--- blast_proteins.py
+++ blast_proteins.py
@@ -6,6 +6,7 @@ faa_files = glob.glob("/Bmo/jyakovleva/metavirome/marine_and_seawater_metagenome
 for pth in faa_files:
     ind = faa_files.index(pth)
     my_cmd = NcbiblastpCommandline(query=pth, db="/Bmo/ncbi_nr_database/nr",
-                                   num_alignments=5, outfmt=5, out='out_blast_' + str(ind) + '.xml', num_threads=16)
+                                   num_alignments=5, outfmt=5,
+                                   out='out_blast_' + str(ind) + '.xml', num_threads=16)
     print(my_cmd)
     stdout, stderr = my_cmd()
