4809b5604b0f17661fd69e149cffc2d834aade7a
Yulia-Yakovleva <st041958@student.spbu.ru>
2020-03-19 23:22:18 +0300

    добавлен тест для минимальной длины

--- tests/test_fastq_filter.py
+++ tests/test_fastq_filter.py
@@ -4,6 +4,15 @@ import fastq_filter
 
 class FastqFilterTest(unittest.TestCase):
 
+    def test_minlen(self):
+        threshhold = 10
+        good_seq = 'GCGCTATAGCGCTATA'
+        bad_seq = 'GCGCTATA'
+        self.assertIsNot(fastq_filter.minlen(threshhold, good_seq)[1], 'looser')
+        self.assertIsNot(fastq_filter.minlen(threshhold, bad_seq)[0], 'looser')
+        self.assertIs(fastq_filter.minlen(threshhold, good_seq)[0], good_seq)
+        self.assertIs(fastq_filter.minlen(threshhold, bad_seq)[1], bad_seq)
+
     def test_gc_content(self):
         seq = 'GCGCTATA'
         self.assertAlmostEqual(fastq_filter.gc_content(seq), 50)
