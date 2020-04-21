d86c164111eb0106d050f69d9e94fb80a3675d7a
Yulia-Yakovleva <st041958@student.spbu.ru>
2020-04-21 04:39:44 +0300

    Оно более менее прилично и работает на тестах

--- PyTrimm/tests.py
+++ PyTrimm/tests.py
@@ -4,57 +4,55 @@ import trimmomatic_clone
 
 class FastqFilterTest(unittest.TestCase):
 
-    def test_minlen_good_seq(self):
-        threshhold = 10
+    def test_minlen_with_good_seq(self):
+        threshold = 10
         seq = 'GCGCTATAGCGCTATA'
-        self.assertIs(trimmomatic_clone.minlen(threshhold, seq)[0], seq)
-        self.assertIs(trimmomatic_clone.minlen(threshhold, seq)[1], 'loser')
+        self.assertTrue(trimmomatic_clone.minlen(seq, threshold))
 
     def test_minlen_bad_seq(self):
-        threshhold = 10
+        threshold = 10
         seq = 'GCGCTATA'
-        self.assertIs(trimmomatic_clone.minlen(threshhold, seq)[0], 'loser')
-        self.assertIs(trimmomatic_clone.minlen(threshhold, seq)[1], seq)
+        self.assertFalse(trimmomatic_clone.minlen(seq, threshold))
 
     def test_gc_content(self):
         seq = 'GCGCTATA'
         self.assertAlmostEqual(trimmomatic_clone.gc_content(seq), 50)
 
-    def test_gc_bounds_one_bound_good_seq(self):
+    def test_gc_content_with_zero_content(self):
+        seq = 'ATATATTATA'
+        self.assertAlmostEqual(trimmomatic_clone.gc_content(seq), 0)
+
+    def test_gc_bounds_one_bound_with_good_seq(self):
         bound = [30]
         seq = 'GCGCTATAGCGCTATA'
-        self.assertIs(trimmomatic_clone.gc_bounds(bound, seq)[0], seq)
-        self.assertIs(trimmomatic_clone.gc_bounds(bound, seq)[1], 'loser')
+        self.assertTrue(trimmomatic_clone.gc_bounds(seq, bound))
 
-    def test_gc_bounds_one_bound_bad_seq(self):
+    def test_gc_bounds_one_bound_with_bad_seq(self):
         bound = [30]
         seq = 'ATATATATATATAT'
-        self.assertIs(trimmomatic_clone.gc_bounds(bound, seq)[0], 'loser')
-        self.assertIs(trimmomatic_clone.gc_bounds(bound, seq)[1], seq)
+        self.assertFalse(trimmomatic_clone.gc_bounds(seq, bound))
 
-    def test_gc_bounds_two_bounds_good_seq(self):
+    def test_gc_bounds_two_bounds_with_good_seq(self):
         bounds = [30, 60]
         seq = 'GCGCTATAGCGCTATA'
-        self.assertIs(trimmomatic_clone.gc_bounds(bounds, seq)[0], seq)
-        self.assertIs(trimmomatic_clone.gc_bounds(bounds, seq)[1], 'loser')
+        self.assertTrue(trimmomatic_clone.gc_bounds(seq, bounds))
 
-    def test_gc_bounds_two_bounds_bad_seq(self):
+    def test_gc_bounds_two_bounds_with_bad_seq(self):
         bounds = [30, 60]
         seq = 'ATATATATATATAT'
-        self.assertIs(trimmomatic_clone.gc_bounds(bounds, seq)[0], 'loser')
-        self.assertIs(trimmomatic_clone.gc_bounds(bounds, seq)[1], seq)
+        self.assertFalse(trimmomatic_clone.gc_bounds(seq, bounds))
 
-    def test_gc_bounds_zero_bounds(self):
+    def test_gc_bounds_without_bounds(self):
         zero_bounds = []
         seq = 'GCGCTATAGCGCTATA'
-        self.assertIsNone(trimmomatic_clone.gc_bounds(zero_bounds, seq))
+        self.assertTrue(trimmomatic_clone.gc_bounds(seq, zero_bounds))
 
-    def test_gc_bounds_rises(self):
+    def test_gc_bounds_error_rise(self):
         more_bounds = [20, 30, 40]
         seq = 'GCGCTATAGCGCTATA'
         # вот так можно проверить, кинет ли ошибку, когда надо ее кидать
         with self.assertRaises(ValueError):
-            trimmomatic_clone.gc_bounds(more_bounds, seq)
+            trimmomatic_clone.gc_bounds(seq, more_bounds)
 
     def test_quality_assessment(self):
         quality = ';;;;;;;;;;;9;7;;.7;393333'
@@ -66,17 +64,33 @@ class FastqFilterTest(unittest.TestCase):
         # первые три нуклеотида имеют плохое качество, проверяем, чтобы они были отброшены
         sequence = 'AAAGTTGCTTCTGGCGTGGGTGGGGGGG'
         quality = '$$$;;;;;;;;;;;9;7;;.7;393333'
-        self.assertEqual(trimmomatic_clone.LEADING(26, sequence, quality),
+        self.assertEqual(trimmomatic_clone.leading(26, sequence, quality),
                          sequence[3:])
 
     def test_trailing(self):
         # последние три нуклеотида имеют плохое качество, проверяем, чтобы они были отброшены
         sequence = 'GTTGCTTCTGGCGTGGGTGGGGGGGAAA'
         quality = ';;;;;;;;;;;9;7;;.7;393333$$$'
-        self.assertEqual(trimmomatic_clone.TRAILING(10, sequence, quality),
+        self.assertEqual(trimmomatic_clone.trailing(10, sequence, quality),
                          sequence[:-3])
 
-    def test_slidingwindow(self):
+    def test_sliding_window(self):
         sequence = 'GGGGGGAAAAAA'
         quality = ';;;;;;$$$$$$'
-        self.assertEqual(trimmomatic_clone.SLIDINGWINDOW(25, 3, sequence, quality), sequence[:6])
+        self.assertEqual(trimmomatic_clone.sliding_window([3, 25], sequence, quality), sequence[:6])
+
+    def test_crop(self):
+        n = 3
+        sequence = 'AGTCACA'
+        self.assertEqual(trimmomatic_clone.crop(n, sequence), 'AGT')
+
+    def test_headcrop(self):
+        n = 3
+        sequence = 'AGTCACA'
+        self.assertEqual(trimmomatic_clone.headcrop(n, sequence), 'CACA')
+
+    def test_check_gc_and_len_surviving(self):
+        sequence = 'ATATATATATATATAT'
+        bounds = [10, 50]
+        n = 0
+        self.assertFalse(trimmomatic_clone.check_gc_and_len_surviving(sequence, bounds, n))
--- PyTrimm/trimmomatic_clone.py
+++ PyTrimm/trimmomatic_clone.py
@@ -10,11 +10,13 @@ def write_records(out_file, rec):
         out_f.write(rec[2] + '\n')
 
 
-def minlen(n, sequence):
+def minlen(sequence, n):
+    if n is None:
+        return True
     if len(sequence) >= n:
-        return sequence, 'loser'
+        return True
     else:
-        return 'loser', sequence
+        return False
 
 
 def gc_content(sequence):
@@ -24,25 +26,24 @@ def gc_content(sequence):
     return round(content)
 
 
-def gc_bounds(bounds_list, sequence):
-    # отсортируем на всякий случай, если юзер перепутает местами большее и
-    # меньшее значение
+def gc_bounds(sequence, bounds_list):
+    if bounds_list is None or len(bounds_list) == 0:
+        return True
+    # отсортируем на всякий случай, если юзер перепутает местами большее и меньшее значение
     bounds_list.sort()
-    if len(bounds_list) == 0:
-        pass  # нам ведь просто с этим ничего не надо делать...
     if len(bounds_list) > 2:
         raise ValueError('The number of borders cannot be more than two')
     if len(bounds_list) == 1:
         if gc_content(sequence) >= bounds_list[0]:
-            return sequence, 'loser'
+            return True
         else:
-            return 'loser', sequence
+            return False
     if len(bounds_list) == 2:
-        if (gc_content(sequence) <= bounds_list[1]) and (
-                gc_content(sequence) >= bounds_list[0]):
-            return sequence, 'loser'
+        if (gc_content(sequence) <= bounds_list[1]) and \
+                (gc_content(sequence) >= bounds_list[0]):
+            return True
         else:
-            return 'loser', sequence
+            return False
 
 
 # вводим правила кодировки качество ASCII->Symbol->Q-score (это phred33 кодировка)
@@ -64,127 +65,154 @@ def count_average_quality(quality):
     return average
 
 
-def HEADCROP(n, sequence):  # cut the specified number of bases from the start of the read
-    return sequence[n:]
+def headcrop(n, sequence):
+    if n is not None:
+        return sequence[n:]
+    else:
+        return sequence
 
 
-def CROP(n, sequence):  # cut the read to a specified length
-    return sequence[:n]
+def crop(n, sequence):
+    if n is not None:
+        return sequence[:n]
+    else:
+        return sequence
 
 
-def TRAILING(threshold, sequence, quality):  # cut bases off the end of a read, if below a threshold quality
-    out_seq = LEADING(threshold, sequence[::-1], quality[::-1])
-    return out_seq[::-1]
+def leading(threshold, sequence, quality):
+    if threshold is not None:
+        for letter in quality:  # пробегаемся с начала последовательности
+            if assess_qscore(letter)[0] >= threshold:  # если наткнулись на значение > или = threshold
+                return sequence[quality.index(letter):]  # возвращаем sequence, начиная с индекса значения
+    else:
+        return sequence
 
 
-def LEADING(threshold, sequence, quality):  # cut bases off the start of a read, if below a threshold quality
-    for letter in quality:  # пробегаемся с начала последовательности
-        if assess_qscore(letter)[0] >= threshold:  # если наткнулись на значение > или = threshold
-            return sequence[quality.index(letter):]  # возвращаем sequence, начиная с индекса значения
+def trailing(threshold, sequence, quality):
+    if threshold is not None:
+        out_seq = leading(threshold, sequence[::-1], quality[::-1])
+        return out_seq[::-1]
+    else:
+        return sequence
 
 
-def SLIDINGWINDOW(threshold, window, sequence, quality):
-    for i in range(0, len(sequence), window):
-        kmer = quality[i:i + window]
-        if count_average_quality(kmer) < threshold:
-            return sequence[:i]
+def sliding_window(params, sequence, quality):
+    if params is not None:
+        window = params[0]
+        threshold = params[1]
+        for i in range(0, len(sequence), window):
+            kmer = quality[i:i + window]
+            if count_average_quality(kmer) < threshold:
+                return sequence[:i]
+    else:
+        return sequence
+
+
+def check_gc_and_len_surviving(sequence, bounds_list, n):
+    if gc_bounds(sequence, bounds_list) and minlen(sequence, n):
+        return True
+    else:
+        return False
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description='Process arguments from user')
-    parser.add_argument(
-        '-min',
-        '--min_length',
-        type=int,
-        help='minimum read length to survive filtration')
-    parser.add_argument(
-        '-k',
-        '--keep_filtered',
-        action='store_true',
-        help='Save reads that do not pass filtering into a separate file')
-    parser.add_argument(
-        '-o',
-        '--output_base_name',
-        help='Specify the prefix of output files')
-    parser.add_argument(
-        '-gc',
-        '--gc_bounds',
-        type=int,
-        nargs='*',
-        help='Number or range of GC-content in reads for surviving. '
-             'One value to indicate a lower threshold.'
-             'Two values to set the minimum and maximum percentage')
+    parser.add_argument('-crop', '--crop', type=int,
+                        help='Cut the read to a specified length.')
+    parser.add_argument('-gc', '--gc_bounds', type=int, nargs='*',
+                        help='Number or range of GC-content in reads for surviving. '
+                             'One value to indicate a lower threshold.'
+                             'Two values to set the minimum and maximum percentage.')
+    parser.add_argument('-headcrop', type=int,
+                        help='Cut the specified number of bases from the start of the read.')
+    parser.add_argument('-k', '--keep_filtered', action='store_true',
+                        help='Save reads that do not pass filtering into a separate file.')
+    parser.add_argument('-leading', '--leading', type=int,
+                        help='Cut bases off the start of a read, if below a threshold quality.')
+    parser.add_argument('-min', '--min_length', type=int,
+                        help='Minimum read length to survive filtration.')
+    parser.add_argument('-o', '--output_base_name',
+                        help='Specify the prefix of output files.')
+    parser.add_argument('-sliding_window', '--sliding_window', type=int, nargs=2,
+                        help='Performs a sliding window trimming approach. It starts scanning at the 5\' end and'
+                             'clips the read once the average quality within the window falls below a threshold.'
+                             'First value to set window size as the number of bases.'
+                             'Second value to set quality threshold.')
     parser.add_argument('input_file', help='Input FASTQ file')
-    parser.add_argument(
-        '-crop', '--crop',
-        type=int,
-        help='Cut the read to a specified length')
-    parser.add_argument(
-        '-leading', '--leading',
-        type=int,
-        help='Cut bases off the start of a read, if below a threshold quality')
-    parser.add_argument(
-        '-trailing', '--trailing',
-        type=int,
-        help='Cut bases off the end of a read, if below a threshold quality')
-    parser.add_argument(
-        '-slidingwindow', '--slidingwindow',
-        type=int,
-        nargs=2,
-        help='Performs a sliding window trimming approach. It starts scanning at the 5\'end and'
-             'clips the read once the average quality within the window falls below a threshold')
+    parser.add_argument('-trailing', '--trailing', type=int,
+                        help='Cut bases off the end of a read, if below a threshold quality.')
     args = parser.parse_args()
 
     if not args.output_base_name:  # разбираемся с названием по умолчанию
         args.output_base_name = args.input_file.rstrip('.fastq')
-        print(args.output_base_name + ' processing... Wait...')
 
-    if os.path.exists(
-            args.output_base_name +
-            '_passed.fq'):  # проверка предсуществующих файлов
+    if os.path.exists(args.output_base_name + '_passed.fq'):  # проверка предсуществующих файлов
         os.remove(args.output_base_name + '_passed.fq')
     if os.path.exists(args.output_base_name + '_failed.fq'):
         os.remove(args.output_base_name + '_failed.fq')
+    if os.path.exists(args.output_base_name):
+        os.remove(args.output_base_name)
 
     with open(args.input_file, "r") as in_f:  # читаем наш fastq файл
+        total_reads = 0
+        passed_reads = 0
+        failed_reads = 0
         for element in in_f:
+            total_reads += 1
             name = element.rstrip()
             seq = next(in_f).rstrip()
             next(in_f)
-            quality = next(in_f).rstrip()
-
-            record = name, seq, quality  # пусть это будет одним рекордом, не хочу ничего потерять
+            qual = next(in_f).rstrip()
 
             # исполнение команд #
+            if leading(args.leading, seq, qual) is not None:
+                bases = leading(args.leading, seq, qual)
+            else:
+                bases = ''
+            if trailing(args.trailing, bases, qual) is not None:
+                bases = trailing(args.trailing, bases, qual)
+            else:
+                bases = ''
+            if sliding_window(args.sliding_window, bases, qual) is not None:
+                bases = sliding_window(args.sliding_window, bases, qual)
+            else:
+                bases = ''
+            if headcrop(args.headcrop, bases) is not None:
+                bases = headcrop(args.headcrop, bases)
+            else:
+                bases = ''
+            if crop(args.crop, bases) is not None:
+                bases = crop(args.crop, bases)
+            else:
+                bases = ''
 
-            if args.min_length and args.gc_bounds and not args.keep_filtered:  # сувмещаем без сохранения
-                if (gc_bounds(args.gc_bounds, record[1])[0] != 'loser') and \
-                        (minlen(args.min_length, record[1])[0] != 'loser'):
-                    write_records(f"{args.output_base_name}_passed.fq", record)
+            # фильтруем и записываем (в дропнутых - исходные риды без тримминга!!!)
+            if len(bases) == 0:  # рид нулевой длины. либо не пишем ничего, либо пишем в дропнутые исходник
+                if args.keep_filtered:
+                    droped_read = name, seq, qual
+                    write_records(f"{args.output_base_name}_failed.fq", droped_read)
+                    failed_reads += 1
+            else:  # рид ненулевой длины, проверяем, пройдет ли фильтрацию?
+                if check_gc_and_len_surviving(seq, args.gc_bounds, args.min_length) is True:
+                    # рид прошел фильтрацию, как сохранять?
+                    surv_read = name, bases, qual
+                    if not args.keep_filtered:
+                        # дропнутые не пишем, просто сохраняем выживший рид без расширения .fq
+                        write_records(f"{args.output_base_name}", surv_read)
+                        passed_reads += 1
+                    else:
+                        # сохраняем выжившие как _passed.fq
+                        write_records(f"{args.output_base_name}_passed.fq", surv_read)
+                        passed_reads += 1
+                elif check_gc_and_len_surviving(seq, args.gc_bounds, args.min_length) is False:
+                    # рид не прошел фильтрацию, как сохранять?
+                    droped_read = name, seq, qual
+                    # сохраняем как _failed.fq
+                    if args.keep_filtered:
+                        write_records(f"{args.output_base_name}_failed.fq", droped_read)
+                        failed_reads += 1
 
-            if args.min_length and args.gc_bounds and args.keep_filtered:  # сувмещаем с сохранением
-                if (gc_bounds(args.gc_bounds, record[1])[0] != 'loser') and \
-                        (minlen(args.min_length, record[1])[0] != 'loser'):
-                    write_records(f"{args.output_base_name}_passed.fq", record)
-                else:
-                    write_records(f"{args.output_base_name}_failed.fq", record)
 
-            if args.min_length and args.keep_filtered and not args.gc_bounds:  # дли минимальной длины с сохранением
-                if minlen(args.min_length, record[1])[0] != 'loser':
-                    write_records(f"{args.output_base_name}_passed.fq", record)
-                else:
-                    write_records(f"{args.output_base_name}_failed.fq", record)
-
-            if args.min_length and not args.keep_filtered and not args.gc_bounds:  # для минимальной длины без сохранения
-                if minlen(args.min_length, record[1])[0] != 'loser':
-                    write_records(f"{args.output_base_name}_passed.fq", record)
-
-            if args.gc_bounds and args.keep_filtered and not args.min_length:  # для ГЦ-контента с сохранинением
-                if gc_bounds(args.gc_bounds, record[1])[0] != 'loser':
-                    write_records(f"{args.output_base_name}_passed.fq", record)
-                else:
-                    write_records(f"{args.output_base_name}_failed.fq", record)
-
-            if args.gc_bounds and not args.keep_filtered and not args.min_length:  # для ГЦ-контента без сохранения
-                if gc_bounds(args.gc_bounds, record[1])[0] != 'loser':
-                    write_records(f"{args.output_base_name}_passed.fq", record)
+        print(f'Всего было обработано {total_reads} ридов. '
+              f'{passed_reads} из них пережили фильтрацию. '
+              f'{failed_reads} рида были отброшены.')
